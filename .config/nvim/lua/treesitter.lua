5717988881c77c65999e1f47f87d34db5ac6b131
David Schatz <david.schatz@tu-ilmenau>
2023-10-14 22:53:53 +0200

    Improved treesitter config

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -1,6 +1,10 @@
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "comment" ,"rust", "bash", "python", "javascript", "typescript" },
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "comment", "rust", "bash", "python", "javascript",
+        "typescript" },
     highlight = {
+        enable = true,
+    },
+    indent = {
         enable = true,
     }
 }
