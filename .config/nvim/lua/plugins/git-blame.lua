aa3bf043d7493a93697e18ee89647849e3795810
David Schatz <david.schatz@tu-ilmenau.de>
2023-11-10 13:59:46 +0100

    Fix blame

--- .config/nvim/lua/plugins/git-blame.lua
+++ .config/nvim/lua/plugins/git-blame.lua
@@ -1,5 +1,6 @@
 return {
-    "FabijanZulj/blame.nvim",
+    "wyv3rn/blame.nvim",
+    branch = "fix-cwd",
     config = function()
         require("blame").setup({
             virtual_style = "right_aligned",
