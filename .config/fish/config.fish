f392dade25134e6eb415c4b7ecf6e49149038fc6
David Schatz <david.schatz@tu-ilmenau.de>
2024-02-19 09:42:16 +0100

    Add keychain to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,16 @@
-bind \cf forward-bigword accept-autosuggestion
-bind \cb backward-bigword
-bind \cn beginning-of-line
-bind \ce end-of-line
-bind \ch backward-kill-bigword
-bind \cg kill-bigword
-
 if status is-interactive
-end
+    bind \cf forward-bigword accept-autosuggestion
+    bind \cb backward-bigword
+    bind \cn beginning-of-line
+    bind \ce end-of-line
+    bind \ch backward-kill-bigword
+    bind \cg kill-bigword
 
-if type -q zoxide
-    zoxide init fish --cmd cd | source
+    if type -q keychain
+        keychain --ignore-missing --eval id_rsa id_ed25519 | source
+    end
+
+    if type -q zoxide
+        zoxide init fish --cmd cd | source
+    end
 end
