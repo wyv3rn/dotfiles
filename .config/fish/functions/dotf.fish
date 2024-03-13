4e0cb2affd7549565a24c880d3837ab1106b0a08
David Schatz <dschatz@davids-mbp.speedport.ip>
2024-03-13 19:57:29 +0100

    Some Mac stuff

--- .config/fish/functions/dotf.fish
+++ .config/fish/functions/dotf.fish
@@ -1,4 +1,3 @@
 function dotf --wraps='/usr/bin/git --git-dir=/home/dschatz/.dotfiles.git/ --work-tree=/home/dschatz' --description 'alias dotf /usr/bin/git --git-dir=/home/dschatz/.dotfiles.git/ --work-tree=/home/dschatz'
-  /usr/bin/git --git-dir=/home/dschatz/.dotfiles.git/ --work-tree=/home/dschatz $argv
-        
+  /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv
 end
