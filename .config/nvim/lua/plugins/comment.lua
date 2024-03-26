85f78f4457a1636a1af8ed617fb12564f6b8a780
David Schatz <david.schatz@tu-ilmenau.de>
2024-03-26 13:46:35 +0100

    Use correct comment style for C++

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,3 +1,19 @@
+-- Source for the preamble: See repo down below.
+
+-- Creates an autocmd group for comment specifications
+vim.api.nvim_create_augroup("comment", { clear = true })
+
+-- Creates an autocmd that runs on BufEnter and BufFilePost
+-- We use the `BufFilePost` trigger so that we can comment after changing file extensions
+-- Without needing to repoen the buffer
+vim.api.nvim_create_autocmd({"BufEnter", "BufFilePost"}, {
+    group = "comment",
+    pattern = {"*.c", "*.h", "*.cpp", "*.hpp"},
+    callback = function()
+        vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+    end
+})
+
 return {
     "terrortylor/nvim-comment",
     config = function()
