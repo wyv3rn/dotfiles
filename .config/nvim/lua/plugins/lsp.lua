b4530eb4ee9f8d1e11f30bbd65468eaa89e0eafa
David Schatz <david.schatz@tu-ilmenau.de>
2024-12-21 14:22:59 +0100

    Switch to blink.nvim and add autoclose

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,10 +6,8 @@ return {
          { 'williamboman/mason.nvim' },
          { 'williamboman/mason-lspconfig.nvim' },
          { 'neovim/nvim-lspconfig' },
-         { 'hrsh7th/nvim-cmp' },
-         { 'hrsh7th/cmp-nvim-lsp' },
-         { 'L3MON4D3/LuaSnip' },
          { 'barreiroleo/ltex_extra.nvim' },
+         { 'saghen/blink.cmp' }
       },
       config = function()
          local lsp_zero = require('lsp-zero')
@@ -18,7 +16,9 @@ return {
 
          -- Default: Install lsp servers with Mason
          require('mason').setup({})
-         require('mason-lspconfig').setup({
+
+         local mason_cfg = require("mason-lspconfig")
+         mason_cfg.setup({
             handlers = {
                lsp_zero.default_setup,
             },
@@ -26,82 +26,6 @@ return {
 
          -- Use system clangd instead of Mason one, because it does not work on Mac silicon
          lsp_config.clangd.setup({})
-
-         -- Configure autocomplete
-         vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-         local luasnip = require("luasnip")
-         local cmp = require("cmp")
-         local select_opts = { behavior = cmp.SelectBehavior.Insert }
-         local confirm_opts = { select = true }
-
-         cmp.setup({
-            snippet = {
-               expand = function(args)
-                  luasnip.lsp_expand(args.body)
-               end
-            },
-            sources = {
-               { name = 'path' },
-               { name = 'nvim_lsp', keyword_length = 1 },
-               { name = 'buffer',   keyword_length = 3 },
-               { name = 'luasnip',  keyword_length = 2 },
-            },
-            window = {
-               documentation = cmp.config.window.bordered()
-            },
-            formatting = {
-               fields = { 'abbr', 'menu', 'kind' },
-            },
-            mapping = {
-               ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
-               ['<Down>'] = cmp.mapping.select_next_item(select_opts),
-
-               ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
-               ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
-
-               ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-               ['<C-d>'] = cmp.mapping.scroll_docs(4),
-
-               ['<C-g>'] = cmp.mapping.abort(),
-               ['<C-c>'] = cmp.mapping.abort(),
-               ['<CR>'] = cmp.mapping.confirm(confirm_opts),
-
-               ['<C-f>'] = cmp.mapping(function(fallback)
-                  if luasnip.jumpable(1) then
-                     luasnip.jump(1)
-                  else
-                     fallback()
-                  end
-               end, { 'i', 's' }),
-
-               ['<C-b>'] = cmp.mapping(function(fallback)
-                  if luasnip.jumpable(-1) then
-                     luasnip.jump(-1)
-                  else
-                     fallback()
-                  end
-               end, { 'i', 's' }),
-
-               ['<C-space>'] = cmp.mapping(function()
-                  local entries = cmp.get_entries()
-                  if #entries > 0 and (#entries == 1 or entries[1].exact) then
-                     cmp.confirm(confirm_opts)
-                  elseif cmp.visible() then
-                     cmp.select_next_item(select_opts)
-                  else
-                     cmp.complete()
-                  end
-               end, { 'i', 's' }),
-
-               ['<Tab>'] = cmp.mapping(function(fallback)
-                  if cmp.visible() and #cmp.get_entries() > 0 then
-                     cmp.confirm(confirm_opts)
-                  else
-                     fallback()
-                  end
-               end, { 'i', 's' }),
-            },
-         })
 
          -- Configure ltex-ls
          local dict_dir = vim.fn.expand("~") .. "/devops/dictionary/"
@@ -128,6 +52,14 @@ return {
                }
             }
          })
+
+         -- Register autocomplete with language servers
+         local capabilities = require("blink.cmp").get_lsp_capabilities()
+         for _, ls in ipairs(mason_cfg.get_installed_servers()) do
+            lsp_config[ls].setup({
+               capabilities = capabilities
+            })
+         end
       end
    },
 }
