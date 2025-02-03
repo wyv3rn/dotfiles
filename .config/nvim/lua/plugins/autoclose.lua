745aa9aa811a2ef2429987b41508ff93b3752884
David Schatz <david.schatz@tu-ilmenau.de>
2025-02-03 14:31:35 +0100

    Further improve autoclose.nvim config

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -3,11 +3,12 @@ return {
    config = function()
       require("autoclose").setup({
          keys = {
-            ["'"] = { escape = true, close = false, pair = "''"},
+            ["'"] = { escape = true, close = false, pair = "''" },
+            ["$"] = { escape = true, close = true, pair = "$$", enabled_filetypes = { "tex", "markdown" } },
          },
          options = {
             disable_when_touch = true,
-            touch_regex = "[%w%s=%(%[%]%)\"]"
+            touch_regex = "[%w%s=%(%)%[%]{}\"]"
          }
       })
    end
