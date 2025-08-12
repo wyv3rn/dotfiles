0a48b72c055af6f511fa55e1a62b5745b183a4ee
David Schatz <david.schatz@tu-ilmenau.de>
2025-08-12 20:06:42 +0200

    Shorten description for dotf alias

--- .config/fish/functions/dotf.fish
+++ .config/fish/functions/dotf.fish
@@ -1,3 +1,3 @@
-function dotf --wraps='/usr/bin/git --git-dir=/home/dschatz/.dotfiles.git/ --work-tree=/home/dschatz' --description 'alias dotf /usr/bin/git --git-dir=/home/dschatz/.dotfiles.git/ --work-tree=/home/dschatz'
+function dotf --wraps='git' --description 'dotfiles'
   /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv
 end
