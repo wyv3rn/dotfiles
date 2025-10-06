2ca7ef1bf621f07ae3f8efa0c95e237213b1c846
David Schatz <david.schatz@tu-ilmenau.de>
2025-10-06 14:22:54 +0200

    Automatically trigger omnicompletion

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -64,6 +64,17 @@ return {
             vim.lsp.enable(lsp)
          end
 
+         -- Automatically trigger omnicompletion
+         vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("my.lsp", {}),
+            callback = function(args)
+               local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+               if client:supports_method("textDocument/completion") then
+                  vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+               end
+            end
+         })
+
          -- Hammerspoon support
          if vim.fn.executable("hs") == 1 then
             local hs_version = vim.fn.system("hs -c _VERSION"):gsub("[\n\r]", "")
