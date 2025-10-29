a6a021e32df5f41396d8b4a30e0ae1013f51f9fb
David Schatz <david.schatz@tu-ilmenau.de>
2025-10-29 16:19:25 +0100

    Trigger autocompletion on almost every keystroke

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -65,13 +65,34 @@ return {
             vim.lsp.enable(lsp)
          end
 
-         -- Automatically trigger omnicompletion
+         -- Function to enable autocompletion on almost every key stroke for a buffer
+         local function enable_autocompletion(buf, keys)
+            local exclude = { ' ', '(', ')', '[', ']', '"', "'" }
+            vim.api.nvim_create_autocmd('InsertCharPre', {
+               buffer = buf,
+               callback = function()
+                  if vim.fn.pumvisible() == 1 or vim.fn.state('m') == 'm' then
+                     return
+                  end
+                  local char = vim.v.char
+                  if not vim.list_contains(exclude, char) then
+                     local key = vim.keycode(keys)
+                     vim.api.nvim_feedkeys(key, 'm', false)
+                  end
+               end
+            })
+         end
+
+         -- Enable autocompletion for buffers on LspAttach
          vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("my.lsp", {}),
             callback = function(args)
                local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
                if client:supports_method("textDocument/completion") then
-                  vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+                  vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+                  enable_autocompletion(args.buf, "<C-x><C-o>")
+               else
+                  enable_autocompletion(args.buf, "<C-x><C-n>")
                end
             end
          })
