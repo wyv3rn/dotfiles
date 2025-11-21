e190fc157a4b8ff2331e3bf94a709d9bde5870fe
David Schatz <david.schatz@tu-ilmenau.de>
2025-11-21 13:16:39 +0100

    Music alias

---
+++ .config/fish/functions/m.fish
@@ -0,0 +1,3 @@
+function m --wraps='mpv $(find $HOME/Music -name "*.m3u" | fzf)' --description 'alias m=mpv $(find $HOME/Music -name "*.m3u" | fzf)'
+    mpv $(find $HOME/Music -name "*.m3u" | fzf) $argv
+end
