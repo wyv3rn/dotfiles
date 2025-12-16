02127e260efd9f1be71bdefce9c7cf9a43b5763a
David Schatz <david.schatz@tu-ilmenau.de>
2025-12-16 15:04:51 +0100

    Improve autoclose

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,16 +1,71 @@
 -- Autoclose
-local pairs = {
+local open_to_pair = {
    ["{"] = "{}",
    ["("] = "()",
    ["["] = "[]",
 }
+
+local close_to_pair = {
+   ["}"] = "{}",
+   [")"] = "()",
+   ["]"] = "[]",
+}
+
+local function str_at(s, i)
+   return string.sub(s, i, i)
+end
+
+local function feed_keys(str)
+   local keys = vim.api.nvim_replace_termcodes(str, true, false, true)
+   vim.api.nvim_feedkeys(keys, "n", false)
+end
+
+-- get some info about where we are at
+local function pos()
+   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+   local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
+   local line_len = #line
+   local at_line_end = col == line_len
+   local in_pair = false
+   local next_char = nil
+   if col > 0 and col < line_len then
+      next_char = str_at(line, col + 1)
+      for _, pair in pairs(open_to_pair) do
+         if str_at(line, col) == str_at(pair, 1) and str_at(line, col + 1) == str_at(pair, 2) then
+            in_pair = true
+         end
+      end
+   end
+   return at_line_end, in_pair, next_char
+end
+
+-- autoclose
 vim.api.nvim_create_autocmd("InsertCharPre", {
    callback = function()
-      local pair = pairs[vim.v.char]
-      if pair then
-         local keys = vim.api.nvim_replace_termcodes("<Left>", true, false, true)
-         vim.api.nvim_feedkeys(keys, "n", false)
+      local at_line_end, in_pair, next_char = pos()
+      local pair = open_to_pair[vim.v.char]
+
+      local autoclose = pair ~= nil and (at_line_end or in_pair)
+      local skip_closing = close_to_pair[vim.v.char] ~= nil and next_char == vim.v.char
+
+      if skip_closing then
+         feed_keys("<Right>")
+         vim.v.char = ''
+         return
+      elseif autoclose then
+         feed_keys("<Left>")
          vim.v.char = pair
       end
    end
 })
+
+-- smart newline matching autoclose
+local function smart_newline()
+   feed_keys("<CR>")
+   local _, in_pair, _ = pos()
+   if in_pair then
+      feed_keys("<C-o>O")
+   end
+end
+
+vim.keymap.set("i", "<CR>", smart_newline)
