3a6ff8af8f0761a4982e607dcd3f4ac82a61d4ba
David Schatz <david.schatz@tu-ilmenau.de>
2026-04-05 11:23:14 +0200

    Fix snippets for nvim 0.12

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -1,5 +1,5 @@
 -- Snippets via registers
-vim.fn.setreg("i", "\\begin\022{itemize\022}\r\\end\022{itemize\022}\027Vk=o\\item ", "c")
-vim.fn.setreg("e", "\\begin\022{enumerate\022}\r\\end\022{enumerate\022}\027Vk=o\\item ", "c")
-vim.fn.setreg("m", "\\begin\022{equation\022}\r\\end\022{equation\022}\027Vk=o", "c")
-vim.fn.setreg("f", "\\begin\022{figure\022}\r\\end\022{figure\022}\027Vk=o\\centering\r\\includegraphics[width=1.0\\linewidth]\022{\022}\r\\caption{}\r\\label\022{\022}\027kkf{a", "c")
+vim.fn.setreg("i", "i\\begin\022{itemize\022}\027o\\end\022{itemize\022}\027Vk=o\\item ", "c")
+vim.fn.setreg("e", "i\\begin\022{enumerate\022}\027o\\end\022{enumerate\022}\027Vk=o\\item ", "c")
+vim.fn.setreg("m", "i\\begin\022{equation\022}\027o\\end\022{equation\022}\027Vk=o", "c")
+vim.fn.setreg("f", "i\\begin\022{figure\022}\027o\\end\022{figure\022}\027Vk=o\\centering\027o\\includegraphics[width=1.0\\linewidth]\022{\022}\027o\\caption{}\027o\\label\022{\022}\027kkf{a", "c")
