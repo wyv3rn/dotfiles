a2237ab3ba005142c82cd942a69e6ca605bec2d6
David Schatz <david.schatz@tu-ilmenau.de>
2026-04-08 17:29:58 +0200

    Treesitter for bibtex

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -8,11 +8,13 @@ local langs = {
    "javascript",
    "typescript",
    "go",
+   "bibtex",
 }
-
 treesitter.install(langs)
 
 table.insert(langs, 1, "tex")
+table.insert(langs, 1, "bib")
+
 vim.api.nvim_create_autocmd("FileType", {
    pattern = langs,
    callback = function() vim.treesitter.start() end
